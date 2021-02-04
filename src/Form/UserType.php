bc0ee44f5f971c6b649f50da287341ddfb4eb832
Mrick <p.hoareau@rt-iut.re>
2021-02-04 11:03:57 +0400

    user index

---
+++ src/Form/UserType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', TextType::class,  ['attr'=>['class'=>'form-control']])
+            ->add('roles', ChoiceType::class,['attr'=>['class'=>'form-control'],'choices'=>[
+                'Utilisateur'=>'ROLE_USER','Administrateur'=>'ROLE_ADMIN'
+            ],'multiple'=>true])
+            ->add('password', TextType::class,  ['attr'=>['class'=>'form-control']])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
